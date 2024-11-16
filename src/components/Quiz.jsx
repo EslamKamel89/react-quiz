0f03786d0529527e01707e42bf0791290353727d
EslamKamel89 <elsamkamelforex@gmail.com>
2024-11-16 16:30:34 +0200

    Quiz component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,4 +1,27 @@
+import { useState } from "react";
+import consoleLog from "../consoleLog.jsx";
+import QUESTIONS from '../questions.js';
+
 export default function Quiz() {
-    return <>
-    </>;
+    const [userAnswers, setUserAnswers] = useState([]);
+    const activeQuestionIndex = userAnswers.length;
+
+    function handleSelectAnswer(answer) {
+        setUserAnswers((prevState) => {
+            return consoleLog([...prevState, answer]);
+        });
+    }
+
+    return <div id="quiz">
+        <div id="question">
+            <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+            <ul id="answers">
+                {QUESTIONS[activeQuestionIndex].answers.map((answer) => {
+                    return <li key={answer} className="answer">
+                        <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                    </li>
+                })}
+            </ul>
+        </div>
+    </div>;
 }
